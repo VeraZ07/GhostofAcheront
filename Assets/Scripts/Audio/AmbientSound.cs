a9198c16fc399daef2299dbdefc7e688f2d9aa2e
Pieruigi <pierluigi.zomparelli@gmail.com>
2023-05-03 00:02:04 +0200

    sound wip

--- Assets/Scripts/Audio/AmbientSound.cs
+++ Assets/Scripts/Audio/AmbientSound.cs
@@ -9,16 +9,48 @@ namespace GOA.Audio
         [SerializeField]
         AudioSource ambientSource;
 
+        [SerializeField]
+        AudioSource waterSource;
+
+        float waterTimeMin = 20f;
+        float waterTimeMax = 40f;
+        float waterTime = 0;
+        bool playing = false;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            //waterTime = Random.Range(waterTimeMin, waterTimeMax);
         }
 
         // Update is called once per frame
         void Update()
         {
+            //CheckWater();
+        }
 
+        void CheckWater()
+        {
+            if (!waterSource.isPlaying)
+            {
+                if (playing)
+                {
+                    playing = false;
+                    waterTime = Random.Range(waterTimeMin, waterTimeMax);     
+                }
+                else
+                {
+                    if (waterTime < 0)
+                    {
+                        playing = true;
+                        waterSource.Play();
+                    }
+                    else
+                    {
+                        waterTime -= Time.deltaTime;
+                    }
+                }
+            }
         }
     }
 
