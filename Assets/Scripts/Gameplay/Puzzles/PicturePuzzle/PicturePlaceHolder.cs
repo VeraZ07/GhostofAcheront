ef75465fa30e1dc44e1f203449a9589521ff8ada
Pieruigi <pierluigi.zomparelli@gmail.com>
2023-05-05 15:53:35 +0200

    added the place holder controller on the picture puzzle

---
+++ Assets/Scripts/Gameplay/Puzzles/PicturePuzzle/PicturePlaceHolder.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GOA
+{
+    public class PicturePlaceHolder : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnEnable()
+        {
+            // Play some clip
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+    }
+
+}
