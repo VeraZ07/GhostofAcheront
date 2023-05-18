5ad88c38039a0db933729b20adebb0c4cbc3e533
Pieruigi <pierluigi.zomparelli@gmail.com>
2023-05-18 22:31:42 +0200

    icon, version ui, jack removed

---
+++ Assets/Scripts/UI/Main/VersionUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace GOA.UI
+{
+    public class VersionUI : MonoBehaviour
+    {
+        [SerializeField]
+        TMP_Text version;
+
+        private void Awake()
+        {
+            version.text = Application.version;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
