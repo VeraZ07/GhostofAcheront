af345abed09243ba7a45c062e57bd12d92bd610f
Pieruigi <pierluigi.zomparelli@gmail.com>
2023-07-03 14:24:40 +0200

    exported puzzle fbx ( 0_Exporting folder )

---
+++ Assets/Scenes/Pier/TestMazeMG.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMazeMG : MonoBehaviour
+{
+    [SerializeField]
+    GameObject floor;
+
+    [SerializeField]
+    float maxSpeed = 2f;
+
+    [SerializeField]
+    float maxResetSpeed = 1f;
+
+    float horizontal = 0; 
+    float vertical = 0;
+
+    float maxAngle = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKey)    
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
